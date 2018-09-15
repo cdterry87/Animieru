3259f602d0290587f01d3b9520de56a0185c18b4
Chase Terry <chase.terry87@gmail.com>
2018-09-15 00:00:32 -0500

    Created a basic layout for the app.  Values are hard-coded for now

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <section class="hero is-info is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title has-text-centered">VidQuery</h1>
+                    <h2 class="title is-5 has-text-centered">Search for your favorite Movies and TV Shows using TVDB and IMDB!</h2>
+                    <div class="columns">
+                        <div class="column is-6 is-offset-3">
+                            <MediaSearch />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <MediaList />
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MediaSearch from './components/MediaSearch.vue'
+import MediaList from './components/MediaList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MediaSearch,
+    MediaList
   }
 }
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  background: #f2f6fa !important;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MediaSearch.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="media-search">
+        <div class="box">
+            <div class="field has-addons">
+                <div class="control has-icons-left is-expanded">
+                    <input type="text" class="input is-medium" placeholder="Search for Movies or TV Shows!">
+                    <span class="icon is-left"><i class="fas fa-search"></i></span>
+                </div>
+                <div class="control">
+                    <a href="" class="button is-info is-medium">Search</a>
+                </div>
+            </div>    
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: 'MediaSearch',
+}
+</script>
+
+<style scoped>
+</style>
