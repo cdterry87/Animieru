2c6e50e60116ba621963ddd63614cc47f055103f
Chase Terry <chase.terry87@gmail.com>
2018-09-24 16:09:41 -0500

    Anime page now displays related shows, episodes, and staff.  These will eventually be separated out into their own files.

--- src/components/ViewAnime.vue
+++ src/components/ViewAnime.vue
@@ -51,7 +51,66 @@
                         </div>
                     </div>
                 </div>
-                
+
+                <div class="box">
+                    <div class="line">
+                        <span class="title is-5">Related</span>
+                    </div>
+                    <div v-for="related in details.related" :key="related.mal_id">
+                        <div v-for="rel in related" :key="rel.mal_id">
+                            <router-link :to="'/' + rel.type + '/' + rel.mal_id">
+                                <strong>{{ rel.title }}</strong> 
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="line">
+                        <span class="title is-5">Episodes</span>    
+                    </div> 
+                    <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Aired Date</th>
+                            </tr>
+                            <tr v-for="episode in details.episode" :key="episode.id">
+                                <td>{{ episode.id }}</td>
+                                <td>{{ episode.title }}</td>
+                                <td>{{ episode.aired }}</td>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+                <div class="box">
+                    <div class="line">
+                        <span class="title is-5">Staff / Voice Actors</span>    
+                    </div> 
+                    <div class="columns is-multiline">
+                        <div class="column is-one-third" v-for="actor in actors" :key="actor.mal_id">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by5">
+                                    <img :src="actor.image_url" class="image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p class="title is-4">{{ actor.name }}</p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        {{ actor.role }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -66,13 +125,23 @@ export default {
   data() {
       return {
           details: '',
+          actors: '',
       }
   },
   created() {
-    //Getting the anime details.
-    axios.get('https://api.jikan.moe/anime/' + this.id)
+    //Getting the anime details + episodes.
+    axios.get('https://api.jikan.moe/anime/' + this.id + '/episodes')
     .then(response => {
         this.details = response.data
+    })
+    .catch(error => {
+        console.log(error)
+    });
+
+    //Getting the anime details + voice actors.
+    axios.get('https://api.jikan.moe/anime/' + this.id + '/characters_staff')
+    .then(response => {
+        this.actors = response.data.staff
     })
     .catch(error => {
         console.log(error)
@@ -89,11 +158,6 @@ span.tag {
 
 div.line {
     margin-bottom: 8px;
-}
-
-.image {
-    border-radius: 5px;
-    box-shadow: 1px 3px 5px #999;
 }
 
 .return-icon {
