2cef1456116869ce717f342df9276f3fe6c5cb43
Chase Terry <chase.terry87@gmail.com>
2018-09-26 00:05:48 -0500

    Placed a message on the initial search page when a search hasn't been performed

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,9 +1,17 @@
 <template>
     <div id="search-results" class="columns">
         <div class="column is-8 is-offset-2">
-            <h2 class="title is-5 has-text-centered">Search Results <span v-if="searchResults.length !== 0"> for "{{ searchCriteria }}"</span></h2>
+            <div v-if="searchResults.length === 0" class="has-text-centered">
+                <span class="fas fa-magic"></span>
+                <div class="subtitle is-4">
+                    Your search results will appear below!
+                </div>
+            </div>
+            <div v-else>
+                <h2 class="title is-4 has-text-centered">Search Results <span v-if="searchResults.length !== 0"> for "{{ searchCriteria }}"</span></h2>
 
-            <SearchResultsItem :searchResults="searchResults" :searchType="searchType" />
+                <SearchResultsItem :searchResults="searchResults" :searchType="searchType" />
+            </div>
         </div>
     </div>
 </template>
@@ -23,3 +31,9 @@ name: 'SearchResults',
 }
 </script>
 
+<style scoped>
+.fas {
+    font-size: 75px !important;
+    margin: 25px 0;
+}
+</style>
