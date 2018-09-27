6402d5b9553365768e379996a07001d821edee05
= <chase.terry87@gmail.com>
2018-09-27 00:08:00 -0500

    Added .env and .htaccess file for production mode deployment.

--- src/components/ActionBar.vue
+++ src/components/ActionBar.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="action-bar">
-        <div class="return-icon button is-info" @click="$router.go(-1)">
+        <button class="return-icon button is-info" @click="$router.go(-1)">
             <i class="fa fa-arrow-left"></i> Go Back
-        </div>
-        <div class="return-icon button is-warning" @click="$router.push('/')">
+        </button>
+        <button class="return-icon button is-warning" @click="$router.push('/')">
             <i class="fa fa-search"></i> Search
-        </div>
-        <div class="return-icon button is-danger" @click="$router.go(+1)">
+        </button>
+        <button class="return-icon button is-danger" @click="$router.go(+1)">
             <i class="fa fa-arrow-right"></i> Go Forward
-        </div>
+        </button>
     </div>
 </template>
 
