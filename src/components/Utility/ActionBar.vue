e0eaa84afd09bc0841d86012efc72e0634b485c4
= <chase.terry87@gmail.com>
2019-07-05 15:23:43 -0500

    Updated navigation

--- src/components/Utility/ActionBar.vue
+++ src/components/Utility/ActionBar.vue
@@ -1,26 +1,45 @@
 <template>
-    <div id="action-bar" class="navbar field is-grouped is-grouped-centered">
-        <div class="control">
-            <button class="return-icon button is-info" @click="$router.go(-1)">
-                <i class="fa fa-arrow-left"></i> Back
-            </button>
-        </div>
-        <div class="control">
-            <button class="return-icon button is-warning" @click="$router.push('/')">
-                <i class="fa fa-search"></i> Search
-            </button>
-        </div>
-        <div class="control">
-            <button class="return-icon button is-danger" @click="$router.go(+1)">
-                Forward<i class="fa fa-arrow-right"></i>
-            </button>
+    <div id="action-bar" class="navbar field is-grouped">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" @click="home">
+                    Animieru
+                </a>
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu is-active">
+                <div class="navbar-start">
+                    <div class="navbar-item">
+                        <a @click="$router.push('/')">
+                            <i class="fa fa-search"></i> Search
+                        </a>
+                    </div>
+                </div>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <a @click="$router.go(-1)">
+                            <i class="fa fa-arrow-left"></i> Back
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'ActionBar'
+    name: 'ActionBar',
+    methods: {
+        home() {
+            this.$store.dispatch('clearSearchResults');
+            this.$router.push('/')
+        }
+    }
 }
 </script>
 
